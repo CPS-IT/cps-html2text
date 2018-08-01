3953fec64d60d4d7743569b4f06e3a97cac3382d
Vladimir Falcon Piva <falcon@cps-it.de>
2018-08-01 10:00:55 +0200

    [TASK] Added basic extension structure

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension CPSIT\CpsHtml2Text config file for ext "cps_html2text".
+ *
+ * Auto generated 16-04-2018 10:55
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'HTML to text converter',
+    'description' => 'HTML to text converter for TYPO3. Based on https://github.com/soundasleep/html2text',
+    'category' => 'misc',
+    'author' => 'Vladimir Falcón Piva',
+    'author_email' => 'falcon@cps-it.de',
+    'state' => 'alpha',
+    'uploadfolder' => false,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.0.1',
+    'constraints' =>
+        [
+            'depends' =>
+                [
+                    'typo3' => '7.6.0-8.7.99',
+                ],
+            'conflicts' =>
+                [
+                ],
+            'suggests' =>
+                [
+                ],
+        ],
+    'clearcacheonload' => false,
+    'author_company' => null,
+];
+
