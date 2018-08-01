eb0abffadfe1f73ad9a793cf4bc487f7e63f572e
Vladimir Falcon Piva <falcon@cps-it.de>
2018-08-01 21:25:05 +0200

    [TASK] Copyright and License

--- Classes/Utility/Html2Text.php
+++ Classes/Utility/Html2Text.php
@@ -1,6 +1,18 @@
 <?php
 namespace CPSIT\CpsHtml2Text\Utility;
 
+/*
+ * This file is part of the cps_html2text project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ */
+
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 /**
